f95c411cd1a0c457863b949f5800298296b4c03b
Jhon <jhon@lepidus.com.br>
2023-11-07 16:19:05 -0400

    Finished integration tests
    Issue: documentacao-e-tarefas/scielo#608
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test02_authorHistory.spec.js
+++ cypress/tests/Test02_authorHistory.spec.js
@@ -1,5 +1,6 @@
 describe('Checks history for an author', function () {
     var submissionData;
+    let submissionFiles;
     
     before(function() {
         submissionData = {
@@ -9,17 +10,60 @@ describe('Checks history for an author', function () {
 			abstract: 'Money: share it fairly, but dont take a slice of my pie',
 			keywords: [
 				'money'
-			],
-            files: [
-                {
-                    'file': 'dummy.pdf',
-                    'fileName': 'dummy.pdf',
-                    'mimeType': 'application/pdf',
-                    'genre': Cypress.env('defaultGenre')
-                }
-            ]
-        }
+			]
+        };
+
+        submissionFiles = [
+            {
+                'file': 'dummy.pdf',
+                'fileName': 'dummy.pdf',
+                'mimeType': 'application/pdf',
+                'genre': 'Article Text'
+            }
+        ]
     });
+
+    function uploadSubmissionFiles(files) {
+        cy.intercept("POST", /submissions\/\d+\/files$/).as('fileUploaded');
+        cy.intercept("POST", /submissions\/\d+\/files\/\d+/).as('genreDefined');
+    
+        files.forEach(file => {
+            cy.fixture(file.file, 'base64').then(fileContent => {
+                cy.get('input[type=file]').attachFile(
+                    {
+                        fileContent,
+                        encoding: 'base64',
+                        filePath: file.fileName,
+                        mimeType: file.mimeType,
+                    }
+                );
+                cy.wait('@fileUploaded').then(({response}) => {
+                    expect(response.statusCode).to.eq(200)
+                });
+                cy.contains('button', file.genre).last().click({force: true});
+                cy.wait('@genreDefined').then(({response}) => {
+                    expect(response.statusCode).to.eq(200)
+                });
+    
+                cy.contains('What kind of file is this?').should('not.exist');
+                cy.contains('.listPanel__item', file.fileName);
+                cy.contains('.pkpBadge', file.genre);
+            });
+        });
+    }
+
+    function finishSubmission(submissionId) {
+        cy.visit('/index.php/publicknowledge/submission?id=' + submissionId);
+        cy.contains('button', 'Continue').click();
+        uploadSubmissionFiles(submissionFiles);
+        cy.contains('button', 'Continue').click();
+        cy.contains('button', 'Continue').click();
+        cy.contains('button', 'Continue').click();
+        cy.contains('button', 'Submit').click();
+        cy.get('.modal__panel:visible').within(() => {
+            cy.contains('button', 'Submit').click();
+        });
+    }
 
     it('Creates new submission for an author', function() {
         cy.login('zwoods', null, 'publicknowledge');
@@ -27,18 +71,22 @@ describe('Checks history for an author', function () {
         cy.window()
 			.then(() => {
 				return cy.createSubmissionWithApi(submissionData, this.csrfToken);
-			})
-			.then(xhr => {
-				return cy.submitSubmissionWithApi(submissionData.id, this.csrfToken);
 			});
+		cy.get('@submissionId').then((submissionId) => {
+            submissionData.id = submissionId;
+            finishSubmission(submissionId);
+        });
     });
     it('Publishes new submission', function() {
         cy.findSubmissionAsEditor('dbarnes', null, 'Woods');
         
         if (Cypress.env('contextTitles').en_US !== 'Public Knowledge Preprint Server') {
-            cy.recordEditorialDecision('Accept and Skip Review');
+            cy.get('li a:contains("Accept and Skip Review")').click();
+            cy.contains('button', 'Skip this email').click();
+            cy.contains('button', 'Record Decision').click();
+            cy.contains('a', 'View Submission').click();
             cy.get('li.ui-state-active a:contains("Copyediting")');
-            cy.get('button[id="publication-button"]').click();
+            cy.get('#publication-button').click();
             cy.get('div#publication button:contains("Schedule For Publication")').click();
             cy.wait(1000);
             
@@ -46,7 +94,7 @@ describe('Checks history for an author', function () {
             cy.get('div[id^="assign-"] button:contains("Save")').click();
         } else {
 			cy.get('#publication-button').click();
-			cy.get('.pkpPublication > .pkpHeader > .pkpHeader__actions > .pkpButton').click();
+			cy.get('div#publication button:contains("Post")').click();
 		}
 
         cy.get('div.pkpWorkflow__publishModal button:contains("Publish"), .pkp_modal_panel button:contains("Post")').click();
